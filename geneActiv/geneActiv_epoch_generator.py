2ce6c869d5652c150cbee89ae1684ec518b89c45
Dimitrios Psaltos <dimitrios.psaltos@pfizer.com>
2022-01-21 10:38:57 -0500

    created geneactiv module

---
+++ geneActiv/geneActiv_epoch_generator.py
@@ -0,0 +1,228 @@
+from gaitpy.gait import Gaitpy
+import os
+import pandas as pd
+import numpy as np
+import logging
+import tailer as tl
+import datetime
+from pathlib import Path
+from multiprocessing import Pool
+#from src.helpers import read_QC
+import sys
+import skdh
+import re
+from tqdm import tqdm
+
+###### Need to use gaitpyV1 environment #######
+
+class EpochError(Exception):
+    pass
+
+def create_epochTimes(time, start, end, epoch_level=60):
+    '''
+    This function creates windows for the epochs, given the start, end, and the epoch level
+
+    :param start: start time of recording
+    :param end: end time of recording
+    :param epoch_level: epoch level in seconds (ie. 60) for 60s epochs
+    :return:
+    '''
+    if epoch_level == 60:
+        periods = pd.date_range(start = start, end = end, freq='T')
+    else:
+        raise EpochError('epoch value not valid')
+
+    return periods
+
+def createEpochHeader(file):
+    '''
+
+    :param file:
+    :return:
+    '''
+    #get header
+    with open(file, mode='rb') as file:
+        fileContent = file.read()
+
+    str_file = fileContent[:2000].decode('ascii')
+
+    # load_template
+    # load_template
+    d = pd.read_csv('GA_Epoch_Template.csv', nrows=99, sep='\t+|,|', header = None)
+
+    #pull data from .bin header
+    devID_obj = re.search('Device Unique Serial Code', str_file)
+    start, end = devID_obj.span()
+    devID = int(str_file[start:end + 10].split('\r')[0].split(':')[1])
+
+    # dev loc
+    loc_obj = re.search('Device Location Code', str_file)
+    start, end = loc_obj.span()
+    dev_loc = str_file[start:end + 12].split('\r')[0].split(':')[1]
+
+    calDate_obj = re.search('Calibration Date', str_file)
+    start, end = calDate_obj.span()
+    calDate = str(str_file[start:end + 30].split('\r')[0]).split(':',1)[1]
+
+    TZ_obj = re.search('Time Zone', str_file)
+    start, end = TZ_obj.span()
+    TZ = str(str_file[start:end + 12].split('\r')[0]).split(':', 1)[1]
+
+    # sampling rate
+    # Focus on the 1st page
+    page1_start = re.search('Recorded Data', str_file)
+    start, end = page1_start.span()
+    page1_str = str_file[start:]
+    start, end = re.search('Measurement Frequency', page1_str).span()
+    freq_rate = float(page1_str[start:end + 10].split('\r')[0].split(':', 1)[1])
+    freq_rate = '{} Hz'.format(str(freq_rate))
+
+    # start time
+    start, end = re.search('Page Time', page1_str).span()
+    start_time_str = page1_str[start:end + 25].split('\r')[0].split(':', 1)[1]
+
+    # Subject ID
+    subj_obj = re.search('Subject Code', str_file)
+    start, end = subj_obj.span()
+    try:
+        subjID = int(str_file[start:end + 10].split('\r')[0].split(':', 1)[1])
+    except ValueError:
+        subjID = ''
+
+    # study center
+    centre_obj = re.search('Study Centre', str_file)
+    start, end = centre_obj.span()
+    try:
+        study_centre = int(str_file[start:end + 10].split('\r')[0].split(':', 1)[1])
+    except ValueError:
+        study_centre = ''
+
+    studyCode_obj = re.search('Study Code', str_file)
+    start, end = studyCode_obj.span()
+    try:
+        studyCode = int(str_file[start:end + 10].split('\r')[0].split(':', 1)[1])
+    except ValueError:
+        studyCode = ''
+
+    ConfigTime_obj = re.search('Config Time', str_file)
+    start, end = ConfigTime_obj.span()
+    ConfigTime = str(str_file[start:end + 30].split('\r')[0].split(':', 1)[1])
+
+    ExtractTime_obj = re.search('Extract Time', str_file)
+    start, end = ExtractTime_obj.span()
+    ExtractTime = str(str_file[start:end + 30].split('\r')[0].split(':', 1)[1])
+
+    ExtractNotes_obj = re.search('Extract Notes', str_file)
+    start, end = ExtractNotes_obj.span()
+    ExtractNotes = str(str_file[start:end + 30].split('\r')[0].split(':', 1)[1])
+
+    SubjectNotes_obj = re.search('Subject Notes', str_file)
+    start, end = SubjectNotes_obj.span()
+    SubjectNotes = str(str_file[start:end + 20].split('\r')[0].split(':', 1)[1]).strip()
+
+    # change info to match what you just pulled from the .bin file
+    d = d.set_index(d.iloc[:, 0]).drop(axis=0, columns=0).T
+    d['Device Unique Serial Code'] = devID
+    d['Device Location Code'] = dev_loc
+    d['Calibration Date'] = calDate
+    d['Time Zone'] = TZ
+    d['Measurement Frequency'] = freq_rate
+    d['Start Time'] = start_time_str
+    d['Subject Code'] = subjID
+    d['Study Centre'] = study_centre
+    d['Study Code'] = studyCode
+    d['Config Time'] = ConfigTime
+    d['Extract Time'] = ExtractTime
+    d['Extract Notes'] = ExtractNotes
+    d['Subject Notes'] = SubjectNotes
+
+    d_df = pd.DataFrame(d).T
+    d_df = d_df.reset_index()
+
+    return d_df
+
+def generateEpochMetrics(df, period):
+    '''
+    This function receives a DataFrame as input and generates epoch summary metrics to be returned as row
+    :param sub_df: input dataframe from epoch duration (ex. 60s segment of data)
+    :param period: period of epoch duration
+    :return: row with epoch summary values for segment
+    '''
+
+    #sub_df = df[(df.time >= period) & (df.time < period + pd.to_timedelta(1, unit='T'))]
+    sub_df = df.truncate(period, period + pd.to_timedelta(1, unit='T'))
+
+    # need to create row as a list
+    xmean = np.mean(sub_df.x).round(4)
+    ymean = np.mean(sub_df.y).round(4)
+    zmean = np.mean(sub_df.z).round(4)
+    lux_mean = int(np.mean(sub_df.lux))
+    sum_button = int(np.sum(sub_df.button))
+    mean_temp = np.mean(sub_df.temp).round(1)
+    svm_s1 = np.sqrt(sub_df.x**2 + sub_df.y**2 + sub_df.z**2) - 1
+    SVM = np.sum(np.abs(svm_s1)).round(2)
+    xSTD = np.std(sub_df.x).round(4)
+    ySTD = np.std(sub_df.y).round(4)
+    zSTD = np.std(sub_df.z).round(4)
+    peakLux = int(np.max(sub_df.lux))
+
+    row = [str(period)+':000', xmean, ymean, zmean, lux_mean, sum_button, mean_temp, SVM, xSTD, ySTD, zSTD, peakLux]
+
+    return row
+
+def geneActiv_epoch_generator(file, savename):
+    final_df = createEpochHeader(file)
+    dat = skdh.read.ReadBin(bases=[0], periods=[24]).predict(file=file)
+
+    timestamps = pd.to_datetime(dat['time']*1000000000, format='%Y-%m-%d %H:%M:%S')
+    periods = create_epochTimes(timestamps, timestamps[0], timestamps[-1])
+    accel = pd.DataFrame(dat['accel'])
+    df = pd.DataFrame({'time': timestamps, 'x': accel.iloc[:, 0], 'y': accel.iloc[:, 1], 'z': accel.iloc[:, 2],
+                       'lux': dat['light']})
+    df['button'] = 0
+    df['temp'] = dat['temperature']
+    df = df.set_index(df.time)
+
+    epoch_data = []
+    # group by minute and derive endpoints
+    epoch_data = [generateEpochMetrics(df, p) for p in tqdm(periods)]
+    epoch_df = pd.DataFrame(epoch_data)
+
+    final_df = createEpochHeader(file)
+
+    for i in range(20):
+        final_df = final_df.append(pd.Series(), ignore_index=True)
+
+    final_df = final_df.append(epoch_df, ignore_index = True)
+    final_df.iloc[1:].to_csv(savename, header=None, index=False)
+
+    print()
+    print('{} has been converted to epoch'.format(file))
+    print()
+
+if __name__ == '__main__':
+    file = '/Users/bluesky/Downloads/10031001_back_055673_2021-06-02 13-53-56.bin'
+    final_df = createEpochHeader(file)
+    dat = skdh.read.ReadBin(bases=[0], periods=[24]).predict(file=file)
+
+    timestamps = pd.to_datetime(dat['time']*1000000000, format='%Y-%m-%d %H:%M:%S')
+    periods = create_epochTimes(timestamps, timestamps[0], timestamps[-1])
+    accel = pd.DataFrame(dat['accel'])
+    df = pd.DataFrame({'time': timestamps, 'x': accel.iloc[:, 0], 'y': accel.iloc[:, 1], 'z': accel.iloc[:, 2],
+                       'lux': dat['light']})
+    df['button'] = 0
+    df['temp'] = dat['temperature']
+    df = df.set_index(df.time)
+
+    epoch_data = []
+    # group by minute and derive endpoints
+    epoch_data = [generateEpochMetrics(df, p) for p in tqdm(periods)]
+    epoch_df = pd.DataFrame(epoch_data)
+
+    final_df = createEpochHeader(file)
+
+    for i in range(20):
+        final_df = final_df.append(pd.Series(), ignore_index=True)
+
+    final_df = final_df.append(epoch_df, ignore_index = True)
+    final_df.iloc[1:].to_csv('/Users/bluesky/Desktop/GA_Epoch_Converter_test2.csv', header=None, index=False)
---
+++ geneActiv/geneActiv_macros.py
@@ -0,0 +1,109 @@
+import xlwings as xw
+import pandas as pd
+import os
+import time
+import sys
+sys.path.insert(1, '/Users/psaltd/Documents/GitHub/STEPP/src/')
+from tqdm import tqdm
+import argparse
+from helpers import get_gaMacro_files_AWS, downloads
+
+def geneactiv_everyday_living_macro_csv(filename, save_name):
+    path = "/Users/bluesky/Desktop/GeneActiv_Macros/"
+    #path = '/Users/psaltd/Desktop/Geneactiv_macro/'
+    #macro = path + 'GENEActiv_Everyday_Living_Overview_1.11_IK_test_withModule1_update.xlsm'
+    macro = path + 'GENEActiv_Everyday_Living_Overview_1.11_IK_test_withModule1.xlsm'
+
+    wb = xw.Book(macro)
+    time.sleep(5)
+    #Import data
+    task1 = "ImportDataFile"
+    ExcelMacro = wb.macro(task1)
+    app = xw.apps.active
+    app.DisplayAlerts = False
+
+    ExcelMacro.run(filename)
+
+    # Generate Plots
+    task2 = 'Data_analysis'
+    genplots = wb.macro(task2)
+    genplots.run()
+
+    full_data = wb.sheets[1]
+
+    full_data.book.save(save_name)
+    app.quit()
+
+    time.sleep(10)
+
+def geneactiv_sleep_macro_csv(filename, save_name):
+    path = '/Users/bluesky/Desktop/GeneActiv_Macros/'
+    #path = '/Users/psaltd/Desktop/Geneactiv_macro/'
+    macro = path + 'GENEActiv_Sleep_Macro_5.xlsm'
+
+    wb = xw.Book(macro)
+    time.sleep(5)
+    # Import data
+    task1 = "ImportDataFile"
+    ExcelMacro = wb.macro(task1)
+    app = xw.apps.active
+    app.DisplayAlerts = False
+    ExcelMacro.run(filename)
+
+    # Generate Plots
+    task2 = 'Data_analysis'
+    genplots = wb.macro(task2)
+    genplots.run()
+
+    full_data = wb.sheets[1]
+    full_data.book.save(save_name)
+    time.sleep(15)
+    app.quit()
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Process GA Macros - Cachexia 1009 Study')
+    parser.add_argument('--macrotype', type=str, default='Everyday_living_macro',
+                        help='enter the type of macro (Everyday_living_macro or Sleep_macro)')
+    parser.add_argument('--download', type=bool, default=False,
+                        help='Set to True to download new 60s epoch files from AWS')
+    parser.add_argument('--position', type=str, default='wrist',
+                        help='position of GA for macro processing (wrist or lumbar)')
+    args = parser.parse_args()
+
+    #to download new files
+    if args.download == True:
+        ga_file = get_gaMacro_files_AWS()
+        [downloads(x.s3_obj) for y, x in tqdm(ga_file.iterrows())]
+    else:
+        pass
+
+    #path = '/Users/psaltd/Desktop/Cachexia/data/C3651009/processed/geneactiv/epoch60s/'
+    #path = '/Users/bluesky/Desktop/Cachexia/data/C3651009/processed/geneactiv/epoch60s/'
+    path = '/Volumes/Promise_Pegasus/cachexia_1009/C3651009/processed/geneactiv/epoch60s/'
+    files = os.listdir(path)
+    save_path = '/Users/bluesky/Desktop/Cachexia/processed/'
+    #########
+    macrotype = args.macrotype
+    #########
+
+    for f in tqdm(files):
+        #Isik does not want lumbar files
+        if not args.position in f:
+            continue
+        ##for debugging
+        if f == '10031001_screening_wrist_GA_60sEpoch.csv': #errored file
+            continue
+        [subject, visit, devloc, toss, toss2] = f.split('_')
+        save_name = '{}_{}_{}_{}_processed.csv'.format(subject, visit, devloc, macrotype)
+        if os.path.exists(save_path+save_name):
+            continue
+        else:
+            print('{} is being processed...'.format(f))
+            if macrotype == 'Sleep_macro':
+                geneactiv_sleep_macro_csv(path + f, save_path + save_name)
+            if macrotype == 'Everyday_living_macro':
+                geneactiv_everyday_living_macro_csv(path + f, save_path + save_name)
+        time.sleep(3)
+
+    print('Done!')
